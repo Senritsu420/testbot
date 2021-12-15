acbec57dd3b4381b9c6d41388f85bac0fd7f8aeb
YUTOKUBO2001 <85555822+YUTOKUBO2001@users.noreply.github.com>
2021-12-15 16:10:30 +0900

    Update GitHub.py

--- GitHub.py
+++ GitHub.py
@@ -1,6 +1,7 @@
 import requests
 
+
 URL = 'https://api.github.com/repos/YUTOKUBO2001/testbot/contributors'
 headers = {'Authorization': 'token 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'} # 取得したアクセストークン
 r = requests.get(URL.format(repo), headers=headers)
-return r.json()
+print(r)
