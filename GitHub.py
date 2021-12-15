db35fdeca9613bda85353ad4e57703d90c9c09b3
YUTOKUBO2001 <85555822+YUTOKUBO2001@users.noreply.github.com>
2021-12-15 15:30:06 +0900

    Update GitHub.py

--- GitHub.py
+++ GitHub.py
@@ -1,1 +1,5 @@
+import request
 
+URL = 'https://api.github.com/repos/YUTOKUBO2001/testbot/pulls?&state=closed'
+headers = {'Authorization': 'token xxxxx'} # 取得したアクセストークン
+r = requests.get(URL.format(repo), headers=headers)
