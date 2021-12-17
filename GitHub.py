107e8ba8654674e6af51814d63c97544b42d5a6c
YUTOKUBO2001 <85555822+YUTOKUBO2001@users.noreply.github.com>
2021-12-17 13:31:19 +0900

    Update GitHub.py

--- GitHub.py
+++ GitHub.py
@@ -4,7 +4,7 @@ URL = 'https://api.github.com/repos/YUTOKUBO2001/testbot/contributors'
 #headers = {'Authorization': 'token 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'} # 取得したアクセストークン
 r = requests.get(URL)
 
-if r["contributions"] <= 1:
+if r[0]["contributions"] <= 1:
   print("false")
 else:
   print("true")
