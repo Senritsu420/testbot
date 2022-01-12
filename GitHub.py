eaede92ef8599ae2139e4fea1954f958bcc10740
YUTOKUBO2001 <85555822+YUTOKUBO2001@users.noreply.github.com>
2022-01-12 16:26:25 +0900

    Update GitHub.py

--- GitHub.py
+++ GitHub.py
@@ -2,11 +2,11 @@ import requests
 import sys
 import json
 
-def get(url, headers):
+def get_issue(url, headers):
     result = requests.get(url, headers = headers)
     return result if result.ok else exit(-1)
 
 token = f'token {sys.argv[1]}'
-r = get(sys.argv[2], {'Authorization': token}).json
+r = get_issue(sys.argv[2], {'Authorization': token}).json
 
 print(r)
