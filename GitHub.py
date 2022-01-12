eb42c11faf3b77d22794dcedacae473b091c3c69
YUTOKUBO2001 <85555822+YUTOKUBO2001@users.noreply.github.com>
2022-01-12 14:17:53 +0900

    Update GitHub.py

--- GitHub.py
+++ GitHub.py
@@ -1,7 +1,11 @@
 import requests
+import sys
 import json
 
-url = input()
+def get(url, headers):
+    result = requests.get(url, headers = headers)
+    return result if result.ok else exit(-1)
+
 r = requests.get(url, headers= headers)
 r_json = r.json
 print(r_json)
