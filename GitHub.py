154018db59e7771b5fbf7acf79f0d65691d5f5d5
YUTOKUBO2001 <85555822+YUTOKUBO2001@users.noreply.github.com>
2022-01-14 15:28:58 +0900

    json内の要素利用できるか確認

--- GitHub.py
+++ GitHub.py
@@ -7,7 +7,7 @@ def get(url, headers):
     return result if result.ok else exit(-1)
 
 token = f'token {sys.argv[2]}'
-r = get(sys.argv[3], {'Authorization': token}).json()
+r = get(sys.argv[1], {'Authorization': token}).json()
 num_issue = r['comments']
 print(num_issue)
 
