59fdfa99c4a805fddaad9c0df0747c29e3935554
YUTOKUBO2001 <85555822+YUTOKUBO2001@users.noreply.github.com>
2022-02-04 13:53:25 +0900

    Update GitHub.py

--- GitHub.py
+++ GitHub.py
@@ -21,7 +21,8 @@ contributors = []
 for i in range(len(repo_cons)):
     contributors.append(repo_cons[0]['login'])
     
-
+user_url = r['user']['url']
+user = get(user_url, {'Authorization': token}).json()
 
 if issue_user in contributors:
     output = f"{issue_user}は初めての貢献者ではありません。"
