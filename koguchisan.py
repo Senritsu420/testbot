1c3e4335173d675d5151be10c52ba6912063a061
YUTOKUBO2001 <85555822+YUTOKUBO2001@users.noreply.github.com>
2022-02-04 15:17:02 +0900

    Update koguchisan.py

--- koguchisan.py
+++ koguchisan.py
@@ -5,16 +5,18 @@ from sklearn.model_selection import StratifiedKFold
 from sklearn.ensemble import RandomForestClassifier # ランダムフォレスト
 import pickle
 import warnings
-warnings.simplefilter('ignore', UserWarning)
 
-test = pd.DataFrame({ 'before_user_commits':[0,1,2],
-                      'before_user_issues':[0,1,2],
-                      'before_user_pull_requests':[0,1,2],
-                      'before_user_comment_issue':[0,1,2],
-                      'before_user_comment_pull_request':[0,1,2]})
+def model():
+  warnings.simplefilter('ignore', UserWarning)
 
-test = np.array([[10,8,7,7,7]])
+  test = pd.DataFrame({ 'before_user_commits':[0,1,2],
+                        'before_user_issues':[0,1,2],
+                        'before_user_pull_requests':[0,1,2],
+                        'before_user_comment_issue':[0,1,2],
+                        'before_user_comment_pull_request':[0,1,2]})
 
-dt = pickle.load(open('LTC_prediction_model.binaryfile','rb'))
-predict = dt.predict_proba(test)
-print(predict[:,1])
+  test = np.array([[10,8,7,7,7]])
+
+  dt = pickle.load(open('LTC_prediction_model.binaryfile','rb'))
+  predict = dt.predict_proba(test)
+  return predict[:,1]
